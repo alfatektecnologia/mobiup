e8faa2d6fbdaa7bfaf1fbd48e48459ede9e8c7c0
alfatektecnologia <oliveiraemanoel.br@gmail.com>
2024-11-24 23:03:06 -0300

    Changed  uint8, uint16 to uint

--- Qsito/carnaval.sol
+++ Qsito/carnaval.sol
@@ -26,9 +26,9 @@ contract CarnavalScores is ERC20 {
         uint nota;       
     }
 
-    uint8 internal constant QUANTIDADE_JURADOS = 36;
-    uint8 internal constant QUANTIDADE_QUESITOS = 9;
-    uint8 internal constant QUANTIDADE_ESCOLAS = 15;
+    uint internal constant QUANTIDADE_JURADOS = 36;
+    uint internal constant QUANTIDADE_QUESITOS = 9;
+    uint internal constant QUANTIDADE_ESCOLAS = 15;
 
     Jurado[QUANTIDADE_JURADOS] public quantidadeJurados;
     Escola[QUANTIDADE_ESCOLAS] public escolas;
@@ -38,52 +38,62 @@ contract CarnavalScores is ERC20 {
     mapping(address => Jurado) public jurados;
     mapping(uint => mapping(uint => Nota)) public notasPorEscolaQuesito;  // Mapeamento de notas por escola e quesito
 
-    constructor() ERC20("CarnavalScores", "CCS") {}
-
+    address public owner;
     
-    uint8 private currentJuradoIndex = 0;
-    uint8 private currentEscolaIndex = 0;
-    uint8 private currentQuesitoIndex = 0;
+    constructor() ERC20("CarnavalScores", "CCS") {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Acesso restrito ao proprietario");
+        _;
+    }
+    
+    uint private _currentJuradoIndex = 0;
+    uint private _currentEscolaIndex = 0;
+    uint private _currentQuesitoIndex = 0;
     event JuradoAdicionado(address indexed jurado, string _nome);
     event EscolaAdicionada(uint id, string _nome);
-    event QuesitoAdicionado(uint8 id,string _nome);
+    event QuesitoAdicionado(uint id, string _nome);
 
     // Add new judge
-    function addJurado(string memory _nomeJurado) public returns (uint) {
-        require(currentJuradoIndex < QUANTIDADE_JURADOS, "Quantidade maxima de jurados");
+    function addJurado(string memory _nomeJurado) public onlyOwner returns (uint) {
+        require(_currentJuradoIndex < QUANTIDADE_JURADOS, "Quantidade maxima de jurados");
         jurados[msg.sender] = Jurado({endereco: msg.sender, nome: _nomeJurado});
-        quantidadeJurados[currentJuradoIndex] = Jurado({endereco: msg.sender, nome: _nomeJurado});
-        currentJuradoIndex++;
+        quantidadeJurados[_currentJuradoIndex] = Jurado({endereco: msg.sender, nome: _nomeJurado});
+        _currentJuradoIndex++;
         emit JuradoAdicionado(msg.sender, _nomeJurado);
-        return currentJuradoIndex - 1;
+        return _currentJuradoIndex - 1;
     }
 
     // Add new team
-    function addEscola(string memory _nomeEscola) public returns (uint) {
-        require(currentEscolaIndex < QUANTIDADE_ESCOLAS, "Quantidade maxima de escolas");
-        escolas[currentEscolaIndex]= Escola({id: currentEscolaIndex, nome: _nomeEscola});
-        emit EscolaAdicionada(currentEscolaIndex, _nomeEscola);
-        currentEscolaIndex++;      
-        return currentEscolaIndex - 1;
+    function addEscola(string memory _nomeEscola) public onlyOwner returns (uint) {
+        require(_currentEscolaIndex < QUANTIDADE_ESCOLAS, "Quantidade maxima de escolas");
+        escolas[_currentEscolaIndex] = Escola({id: _currentEscolaIndex, nome: _nomeEscola});
+        emit EscolaAdicionada(_currentEscolaIndex, _nomeEscola);
+        _currentEscolaIndex++;      
+        return _currentEscolaIndex - 1;
     }
 
     // Add new quesito
-    function addQuesito(string memory _nomeQuesito) public returns (uint) {
-        require(currentQuesitoIndex < QUANTIDADE_QUESITOS, "Quantidade maxima de quesitos");
-        quesitos[currentQuesitoIndex] = Quesito({id: currentQuesitoIndex, nome: _nomeQuesito});
-        emit QuesitoAdicionado(currentQuesitoIndex,_nomeQuesito);
-        currentQuesitoIndex++;
-        return currentQuesitoIndex - 1;
+    function addQuesito(string memory _nomeQuesito) public onlyOwner returns (uint) {
+        require(_currentQuesitoIndex < QUANTIDADE_QUESITOS, "Quantidade maxima de quesitos");
+        quesitos[_currentQuesitoIndex] = Quesito({id: _currentQuesitoIndex, nome: _nomeQuesito});
+        emit QuesitoAdicionado(_currentQuesitoIndex, _nomeQuesito);
+        _currentQuesitoIndex++;
+        return _currentQuesitoIndex - 1;
     }
 
     // Save a score
-    function registrarNota(uint8 _escolaId, uint8 _quesitoId, uint16 _nota) public {
+    function registrarNota(uint _escolaId, uint _quesitoId, uint _nota) public {
         require(jurados[msg.sender].endereco == msg.sender, "Somente jurados podem registrar");
         require(_nota >= 900 && _nota <= 1000, "Nota impossivel");
-        Nota memory nota = Nota({escolaId:_escolaId,
-                              quesitoId: _quesitoId,
-                               juradoId :msg.sender,
-                                nota    :_nota});
+        Nota memory nota = Nota({
+            escolaId: _escolaId,
+            quesitoId: _quesitoId,
+            juradoId: msg.sender,
+            nota: _nota
+        });
         notas.push(nota);
         notasPorEscolaQuesito[_escolaId][_quesitoId] = nota;
     }
@@ -95,12 +105,12 @@ contract CarnavalScores is ERC20 {
     }
 
     // Clear all stored data
-    function limparDados() public {
+    function limparDados() public onlyOwner {
         delete quantidadeJurados;
         delete escolas;
         delete quesitos;
         delete notas;
-
+       
         // Reinitializing mappings
         for (uint i = 0; i < QUANTIDADE_ESCOLAS; i++) {
             for (uint j = 0; j < QUANTIDADE_QUESITOS; j++) {
